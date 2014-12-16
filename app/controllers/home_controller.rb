2a564138f79f59b859868c79c1c2a720a9b6d969
Łukasz Korecki <lukasz@coffeesounds.com>
2014-12-16 19:15:56 +0000

    Adding part 1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,21 @@ class HomeController < ApplicationController
   def index
     @ads = Ad.all
   end
+
+  def new
+    @ad = Ad.new(user: current_user)
+  end
+
+  def create
+    @ad = Ad.new(parameters.merge(user: current_user))
+    if @ad.valid? && @ad.save!
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def parameters
+    params.require(:ad).permit(:title, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get '/ads' => 'home#new', as: :ads
+  post '/ads' => 'home#create'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
