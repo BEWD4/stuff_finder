13adbff8d48c36826d6e6efd566a06f0f82db45f
Łukasz Korecki <lukasz@coffeesounds.com>
2014-12-16 19:09:15 +0000

    list all ads

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.first
+
+to_rent = Category.where(name: 'rent').first_or_create!
+for_sale = Category.where(name: 'sale').first_or_create!
+
+[
+  { user: user,
+    category: to_rent,
+    title: 'lovely condo',
+    description: 'lorem ipsum lorem ipsum'
+},
+{ user: user,
+  category: for_sale,
+  title: 'cool flat',
+  description: 'lorem ipsum lorem ipsum'
+},
+].each do |condition|
+  Ad.where(condition).first_or_create
+end
+
